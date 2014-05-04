a2a79af14f2ab374c6a62c30a29ab4e354ff9655
Amit Bader <capt.dreamer@gmail.com>
2014-05-03 22:29:29 -0500

    finish RNG module

--- lib/rot_rails/rng.rb
+++ lib/rot_rails/rng.rb
@@ -8,6 +8,64 @@ module RotRails::RNG
 	@seed = nil
 	@initialized = false
 
+	# * @returns {number} 
+	def self.getSeed()
+		return @seed
+	end
+
+	# * @param {int} lowerBound The lower end of the range to return a value from, inclusive
+	# * @param {int} upperBound The upper end of the range to return a value from, inclusive
+	# * @returns {int} Pseudorandom value [lowerBound, upperBound], using ROT.RNG.getUniform() to distribute the value
+	def self.getUniformInt(lowerBound, upperBound)
+		max = [lowerBound, upperBound].max
+		min = [lowerBound, upperBound].min
+		return (self.getUniform() * (max - min + 1)).floor + min
+	end
+
+	# * @returns {int} Pseudorandom value [1,100] inclusive, uniformly distributed
+	def self.getPercentage()
+		return 1 + (this.getUniform()*100).floor
+	end
+	
+	# * @param {object} data key=whatever, value=weight (relative probability)
+	# * @returns {string} whatever
+	def self.getWeightedValue(data)
+		avail = []
+		total = 0
+		
+		for id in data
+			total += data[id]
+		end
+		random = (self.getUniform()*total).floor
+		
+		part = 0
+		for id in data
+			part += data[id]
+			if (random < part)
+				return id
+			end
+		end
+		
+		return nil
+	end
+
+	# * Get RNG state. Useful for storing the state and re-setting it via setState.
+	# * @returns {?} Internal state
+	def self.getState()
+		return [@s0, @s1, @s2, @c]
+	end
+
+	# * Set a previously retrieved state.
+	# * @param {?} state
+	def self.setState(state)
+		@s0 = state[0]
+		@s1 = state[1]
+		@s2 = state[2]
+		@c  = state[3]
+		@initialized = true
+	end
+
+
 	# * @param {number} seed Seed the number generator
 	def self.setSeed(seed)
 		seed = (seed < 1 ? 1/seed : seed)
---
+++ spec/RotTests/rng_test.rb
@@ -0,0 +1,76 @@
+describe "RNG" do
+	describe "getUniform" do
+		before do
+			@value = RotRails::RNG.getUniform()
+		end
+		it "should return a number" do
+			expect((@value).kind_of?(Float)).to eq true
+		end
+		it "should return a number 0..1" do
+			expect(@value).to be > (0)
+			expect(@value).to be < (1)
+		end
+	end
+
+	describe "getUniformInt" do
+		before do
+			@lowerBound = 5
+			@upperBound = 10
+		end
+		it "should return a number" do
+		    @value = RotRails::RNG.getUniformInt(@lowerBound, @upperBound)
+			expect((@value).kind_of?(Integer)).to eq true
+		end
+		it "should not care which number is larger in the arguments" do
+			seed = (Random.rand(100000)*1000000).round
+			RotRails::RNG.setSeed(seed)
+			val1 = RotRails::RNG.getUniformInt(@lowerBound, @upperBound)
+			RotRails::RNG.setSeed(seed)
+			val2 = RotRails::RNG.getUniformInt(@upperBound, @lowerBound)
+			expect(val1).to eq (val2)
+		end
+		it "should only return a number in the desired range" do
+			value = RotRails::RNG.getUniformInt(@lowerBound, @upperBound)
+			value2 = RotRails::RNG.getUniformInt(@upperBound, @lowerBound)
+			expect(value).to_not be > (@upperBound)
+			expect(value).to_not be < (@lowerBound)
+			expect(value2).to_not be > (@upperBound)
+			expect(value2).to_not be < (@lowerBound)
+		end
+	end
+
+	describe "seeding" do
+		it "should return a seed number" do
+			expect((RotRails::RNG.getSeed()).kind_of?(Integer)).to eq true
+		end
+
+		it "should return the same value for a given seed" do
+			seed = (Random.rand(100000)*1000000).round
+			RotRails::RNG.setSeed(seed)
+			val1 = RotRails::RNG.getUniform()
+			RotRails::RNG.setSeed(seed)
+			val2 = RotRails::RNG.getUniform()
+			expect(val1).to eq (val2)
+		end
+
+		it "should return a precomputed value for a given seed" do
+			RotRails::RNG.setSeed(12345)
+			val = RotRails::RNG.getUniform()
+			expect(val).to eq (0.01198604702949524)
+		end
+	end
+
+	describe "state manipulation" do
+		it "should return identical values after setting identical states" do
+			RotRails::RNG.getUniform()
+
+			state = RotRails::RNG.getState()
+			val1 = RotRails::RNG.getUniform()
+			RotRails::RNG.setState(state)
+			val2 = RotRails::RNG.getUniform()
+
+			expect(val1).to eq (val2)
+		end
+	end
+
+end
